435d6dcc4acede1cc214fc9b3d040b8e3c5ebe96
Johann Völzer <voelzerjohann@gmail.com>
2020-12-05 22:38:08 +0100

    setting up getName

--- frontend/src/pages/details/DetailsPage.js
+++ frontend/src/pages/details/DetailsPage.js
@@ -1,36 +1,22 @@
 import Navigation from '../../components/Navigation';
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import GetName from '../../services/GetName';
 import DetailsHeader from '../../styles/details/DetailsHeader';
 import BackButton from '../../components/BackButton';
 
-const url = 'https://www.alphavantage.co/query'
-const key = process.env.ALPHA_VANTAGE_API_KEY
-
 const DetailsPage = () => {
+
     const {stockSymbol} = useParams();
-
-    const [results, setResults] = useState({});
-
-    function handleClick(event) {
-            event.preventDefault();  
-            axios.get(`${url}?function=OVERVIEW&symbol=IBM&apikey=${key}`)  
-                .then(({ data }) => {
-                setResults(data)
-            })
-    }
-
+    const symbol = {stockSymbol}
 
     return (
         <div>
             <DetailsHeader>
                 <BackButton />
-                <button onClick={handleClick}>Hallo</button>
                 <h2 style={{margin: '0'}}>{stockSymbol}</h2>
-                <p>hi</p>
+                <p>bookmark</p>
             </DetailsHeader>
-            {results.Name}
+                <GetName symbol={symbol} />
             <Navigation />
         </div>
     );
