70f89622b94db0e2b752d38f7f2a0575946f3bc3
Johann Völzer <voelzerjohann@gmail.com>
2020-12-07 12:30:25 +0100

    implementation of back button

--- frontend/src/components/BackButton.js
+++ frontend/src/components/BackButton.js
@@ -2,7 +2,7 @@ import { useHistory } from 'react-router-dom';
 import { BackIcon } from './Icons'; 
 
 export default function BackButton() {
-    let history = useHistory();
+    const history = useHistory();
     return (
     <div onClick={() => history.goBack()}>
         <BackIcon />
